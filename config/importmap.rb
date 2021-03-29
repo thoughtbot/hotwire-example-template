43698b383553d973f12818f6ef34db0dfb07446e
Sean Doyle <sean.p.doyle24@gmail.com>
2021-03-29 10:26:49 -0400

    [GENERATED]: Install ActiveStorage and ActionText
    This commit executes scripts to install and configure both
    [Active Storage][active_storage] and [Action Text][action_text].
    
    The contents of this diff's contents were generated by executing the
    following commands:
    
    ```bash
    bin/rails active_storage:install
    bin/rails action_text:install
    bin/rails db:migrate
    ```
    
    [Action Text]: https://edgeguides.rubyonrails.org/action_text_overview.html#installation
    [Active Storage]: https://edgeguides.rubyonrails.org/active_storage_overview.html

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,5 @@ pin "@hotwired/turbo-rails", to: "turbo.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "trix"
+pin "@rails/actiontext", to: "actiontext.js"
