6b2003e945a5aead01b195f791827ea8ce1a9acb
Sean Doyle <sean.p.doyle24@gmail.com>
2021-08-29 20:39:44 -0400

    [SKIP]: Add User fixture data

--- db/migrate/20210830002723_create_users.rb
+++ db/migrate/20210830002723_create_users.rb
@@ -1,9 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
-    enable_extension "citext"
     create_table :users do |t|
-      t.citext :username
-      t.citext :name
+      t.text :username
+      t.text :name
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema[7.0].define(version: 2022_02_13_162643) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "citext"
   enable_extension "plpgsql"
 
   create_table "action_text_rich_texts", force: :cascade do |t|
@@ -54,10 +53,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_13_162643) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.citext "username"
-    t.citext "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.text "username"
+    t.text "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["username"], name: "index_users_on_username"
   end
 
