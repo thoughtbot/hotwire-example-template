682f071527a165d7f3f3d57f83024d9086e2079d
Sean Doyle <sean.p.doyle24@gmail.com>
2021-10-08 13:49:25 -0400

    Move a card down

--- test/system/boards_test.rb
+++ test/system/boards_test.rb
@@ -12,4 +12,18 @@ class BoardsTest < ApplicationSystemTestCase
     within_section(doing.name) { assert_text write.name }
     within_section(done.name) { assert_text setup.name }
   end
+
+  test "move a Card down a Stage" do
+    todo = stages :todo
+    first, middle, last = cards :edit, :pull_request, :publish
+
+    visit board_path(todo.board)
+    within_section todo.name do
+      click_on "Move #{middle.name} down"
+
+      assert_css "li:nth-of-type(1)", text: first.name
+      assert_css "li:nth-of-type(2)", text: last.name
+      assert_css "li:nth-of-type(3)", text: middle.name
+    end
+  end
 end
