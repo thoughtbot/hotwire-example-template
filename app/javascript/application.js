0f1962a1eaca5d4c053317894317ec92b6fb5a10
Sean Doyle <sean.p.doyle24@gmail.com>
2021-11-06 10:46:45 -0400

    [SKIP]: Rely on Tailwind Just-in-Time CDN
    Rely on the [Tailwind Just-in-Time CDN][] to provide its utility classes
    for one-off styling adjustments. To minimize the amount of work
    necessary to distinguish elements from one another, disable its
    [preflight][] style resets.
    
    [Tailwind Just-in-Time CDN]: https://tailwindcss.com/docs/installation/play-cdn
    [preflight]: https://tailwindcss.com/docs/preflight#disabling-preflight

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "tailwind.config"
 import "@hotwired/turbo-rails"
 import "controllers"
 import "trix"
--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.js"
+pin "tailwind.config"
