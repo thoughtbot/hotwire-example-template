0bf4821f3f8507e8c7ee9a706531a7fde7c24baf
Sean Doyle <sean.p.doyle24@gmail.com>
2022-05-21 11:37:31 -0400

    [SKIP]: Route root to first `Board`
    When visiting the application root route (i.e. `/`), redirect to the
    `boards#index` action then, within the `boards#index` action, redirect
    to the first `Board` record.

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,4 +2,10 @@ class BoardsController < ApplicationController
   def show
     @board = Board.find params[:id]
   end
+
+  def index
+    @boards = Board.all
+
+    redirect_to board_url(@boards.first)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
   resources :boards, only: :show do
     resources :cards, only: :update
   end
+
+  # Defines the root path route ("/")
+  root to: redirect("/boards")
 end
