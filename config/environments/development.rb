3a196c1537fbc036a1909944d068be5ef796564a
Sean Doyle <sean.p.doyle24@gmail.com>
2022-08-15 17:29:39 -0400

    [SKIP]: Configure for `gitpod.io` support
    Inspired by [gitpod-io/template-ruby-on-rails-postgres][].
    
    Add a variety of files for [gitpod.io][] support, including the
    platform-specific `.gitpod.yml` file, and the `.gitpod.Dockerfile`.
    
    Outside of those files, make `gitpod.io`-specific overrides to
    `config/environments/development.rb` to support serving the application
    from a `gitpod.io`-controller subdomain.
    
    [gitpod-io/template-ruby-on-rails-postgres]: https://github.com/gitpod-io/template-ruby-on-rails-postgres
    [gitpod.io]: https://gitpod.io

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,8 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.hosts.clear
+
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
